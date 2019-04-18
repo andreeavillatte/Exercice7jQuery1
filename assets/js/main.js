16a38d655eef80a1ae5ffaa350eca71cea0850c7
core.editor <andreeavillatte@gmail.com>
2019-04-18 11:59:58 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //si je clique sur le bouton
+    $('button').click(function () {
+    // je suprimme les lignes qui ont la classe erase
+        $('.remove').remove();
+    });
+});
